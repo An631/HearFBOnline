21d12893def6387479c82b30096745796e7486c0
user <user@user.(none)>
2012-12-30 11:16:16 -0600

    Changed greetings for the sake of god

--- index.php
+++ index.php
@@ -249,7 +249,7 @@ $app_name = idx($app_info, 'name', '');
     </header>
 
     <section id="get-started">
-      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <p>Hi I am working hard to please your likes with facebook</p>
       <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
     </section>
 
