c891e2e09105cebe890939556155992b6eccef9b
user <user@user.(none)>
2013-01-06 16:12:54 -0600

    conversaciones scrolls working

--- index2.php
+++ index2.php
@@ -137,19 +137,19 @@ if ($user_id)
           xfbml      : true // parse XFBML
         });
 
-      //   // Listen to the auth.login which will be called when the user logs in
-      //   // using the Login button
-      //   FB.Event.subscribe('auth.login', function(response) {
-      //     // We want to reload the page now so PHP can read the cookie that the
-      //     // Javascript SDK sat. But we don't want to use
-      //     // window.location.reload() because if this is in a canvas there was a
-      //     // post made to this page and a reload will trigger a message to the
-      //     // user asking if they want to send data again.
-      //     window.location = window.location;
-      //   });
+        // Listen to the auth.login which will be called when the user logs in
+        // using the Login button
+        FB.Event.subscribe('auth.login', function(response) {
+          // We want to reload the page now so PHP can read the cookie that the
+          // Javascript SDK sat. But we don't want to use
+          // window.location.reload() because if this is in a canvas there was a
+          // post made to this page and a reload will trigger a message to the
+          // user asking if they want to send data again.
+          window.location = window.location;
+        });
 
-      //   FB.Canvas.setAutoGrow();
-      // };
+        FB.Canvas.setAutoGrow();
+      };
 
       // // Load the SDK Asynchronously
       // (function(d, s, id) {
