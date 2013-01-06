e77c38be9db22b164e3c6d27e5308fa0a4262ea2
user <user@user.(none)>
2013-01-05 20:29:37 -0600

    participantes de los mensajes printed

---
+++ index2.php
@@ -0,0 +1,233 @@
+<html>
+	<head>
+		<title>HearFB</title>
+		<script src="javascript/jquery.js"></script>
+		<script src="javascript/hotkeys.js"></script>
+		<script src="javascript/soundmanager2.js"></script>
+		
+		<link rel="stylesheet" href="stylesheets/home.css" />
+	</head>
+
+<body class="blackBG_whitefont">
+
+	<div id="wrapperMain">
+			
+		<div id="HearFBOnlineTitle">
+			HearFBOnline<div>Facebook for the visually impaired</div>
+		</div><!--HearFBOnlineTitle-->
+
+
+
+
+		
+<div id="threadsContainer">
+		<div id="threadsScroller">
+				<div class="thread">
+					<div class="participantUsers">
+						Angel Duran, Rafa Diaz
+					</div><!--participantUsers-->
+
+					<div   class="grayborder_whitefont messages">
+						<!-- <div id="vScrollMessages"> -->
+							
+							<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+
+									<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+
+									<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+					</div><!--messages-->
+
+				</div><!--thread-->
+
+
+					<div class="thread">
+
+						<div class="participantUsers">
+							Angel Duran, Rafa Diaz
+						</div><!--participantUsers-->
+
+						<div   class="grayborder_whitefont messages">
+							<!-- <div id="vScrollMessages"> -->
+								
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+
+									<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+
+									<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">Rafa Diaz</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato? Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres venir a jugar un rato?</span>
+
+								</div>
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">xD, nada nada y t&uacute;?</span>
+
+								</div>
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Hola que haces? que me platicas? yo estoy bien, jugando al fifa 2012 pero ya se acabo, no quieres ve</span>
+
+								</div>
+								
+
+								<div class="message">
+									<div class="msgHour">2012-12-31 04:47:49</div>
+									<span class="from">angel duran</span>
+									<span class="msgText">Entonces estaba sentado frente a un arbol que me dijo que lo siguiera pero no le quize hacer caso porque me ve&iacute;a raro</span>
+
+								</div>
+						</div><!--messages-->
+					</div><!--thread-->
+				</div><!--threadsScroller-->
+</div><!--threadsContainer-->
+			<div id="containerNuevoMensaje">
+
+			    <input type="text" id="txtNuevoMensaje" />
+			    <input type="button" id="btnSendNuevoMensaje"  value="send"/>
+
+			</div><!--containerNuevoMensaje-->
+
+	</div><!--wrapperMain-->
+
+</body>
+
+
+
+
+
+<script src="javascript/HearFBOnline.js" type="text/javascript"></script>
+</html>
